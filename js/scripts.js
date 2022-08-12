a2dda996c10b6c2d59969b0136857405f55ce129
Stewart <stilondanga@gmail.com>
2022-08-12 14:57:44 +0300

    add js/scripts,js

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ const wave1 = document.querySelector('.circle__back-1');
 const wave2 = document.querySelector('.circle__back-2');
 
 /*  rate slider */
-/*const container = document.querySelector('.slider__box');
+const container = document.querySelector('.slider__box');
 const btn = document.querySelector('.slider__btn');
 const color = document.querySelector('.slider__color');
 const tooltip = document.querySelector('.slider__tooltip');
@@ -35,52 +35,56 @@ clock = () => {
 }
 
 rotation = (target, val) => {
-  target.style.transform =  `rotate(${val}deg)`;
+  target.style.transform = `rotate(${val}deg)`;
 }
 
 window.onload = clock();
 
 dragElement = (target, btn) => {
   target.addEventListener('mousedown', (e) => {
-      onMouseMove(e);
-      window.addEventListener('mousemove', onMouseMove);
-      window.addEventListener('mouseup', onMouseUp);
+    onMouseMove(e);
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
   });
 
   onMouseMove = (e) => {
-      e.preventDefault();
-      let targetRect = target.getBoundingClientRect();
-      let x = e.pageX - targetRect.left + 10;
-      if (x > targetRect.width) { x = targetRect.width};
-      if (x < 0){ x = 0};
-      btn.x = x - 10;
-      btn.style.left = btn.x + 'px';
+    e.preventDefault();
+    let targetRect = target.getBoundingClientRect();
+    let x = e.pageX - targetRect.left + 10;
+    if (x > targetRect.width) {
+      x = targetRect.width
+    };
+    if (x < 0) {
+      x = 0
+    };
+    btn.x = x - 10;
+    btn.style.left = btn.x + 'px';
 
-      // get the position of the button inside the container (%)
-      let percentPosition = (btn.x + 10) / targetRect.width * 100;
+    // get the position of the button inside the container (%)
+    let percentPosition = (btn.x + 10) / targetRect.width * 100;
 
-      // color width = position of button (%)
-      color.style.width = percentPosition + "%";
+    // color width = position of button (%)
+    color.style.width = percentPosition + "%";
 
-      // move the tooltip when button moves, and show the tooltip
-      tooltip.style.left = btn.x - 5 + 'px';
-      tooltip.style.opacity = 1;
+    // move the tooltip when button moves, and show the tooltip
+    tooltip.style.left = btn.x - 5 + 'px';
+    tooltip.style.opacity = 1;
 
-      // show the percentage in the tooltip
-      tooltip.textContent = Math.round(percentPosition) + '%';
+    // show the percentage in the tooltip
+    tooltip.textContent = Math.round(percentPosition) + '%';
   };
 
-  onMouseUp  = (e) => {
-      window.removeEventListener('mousemove', onMouseMove);
+  onMouseUp = (e) => {
+    window.removeEventListener('mousemove', onMouseMove);
+    tooltip.style.opacity = 0;
+
+    btn.addEventListener('mouseover', function() {
+      tooltip.style.opacity = 1;
+    });
+
+    btn.addEventListener('mouseout', function() {
       tooltip.style.opacity = 0;
-
-      btn.addEventListener('mouseover', function() {
-        tooltip.style.opacity = 1;
-      });
-
-      btn.addEventListener('mouseout', function() {
-        tooltip.style.opacity = 0;
-      });
+    });
   };
 };
 

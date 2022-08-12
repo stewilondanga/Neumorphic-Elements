4f77fb671e4c689f88bb97fd0dfd09ae7d96fb47
Stewart <stilondanga@gmail.com>
2022-08-12 14:55:42 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
 
 /*  play button */
-const play = document.querySelector('.play');
+/*const play = document.querySelector('.play');
 const pause = document.querySelector('.pause');
 const playBtn = document.querySelector('.circle__btn');
 const wave1 = document.querySelector('.circle__back-1');
 const wave2 = document.querySelector('.circle__back-2');
 
 /*  rate slider */
-const container = document.querySelector('.slider__box');
+/*const container = document.querySelector('.slider__box');
 const btn = document.querySelector('.slider__btn');
 const color = document.querySelector('.slider__color');
 const tooltip = document.querySelector('.slider__tooltip');
@@ -97,10 +97,10 @@ playBtn.addEventListener('click', function(e) {
 });
 
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
